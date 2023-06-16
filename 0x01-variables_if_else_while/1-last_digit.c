b033b3aeb13b563ee6ac88280c0bc02d2cb8cc80
pdennis45 <peterdennis45@gmail.com>
2023-06-16 02:17:50 -0700

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,13 +1,7 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 
 /* betty style doc for function main goes there */
 int main(void)
@@ -17,15 +11,5 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else
-		printf("%d is zero\n", n);
 	return (0);
 }
