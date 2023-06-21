abc7ef77d7c782b0d67b23a9d4a3bcadf73638e2
pdennis45 <peterdennis45@gmail.com>
2023-06-20 17:37:19 -0700

    12. a function that prints the n times table, starting with 0.

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints the first 50 fibonacci sequence
+ * beginning by 1, 2
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, n;
+	unsigned long prev1, prev2, nextTerim;
+	/** Initializing here because of Betty... */
+	prev1 = 0;
+	prev2 = 1;
+	nextTerm = 0;
+	n = 98;
+	for (i = 1; i <= n; i++)
+	{
+		nextTerm = prev1 + prev2;
+		prev1 = prev2;
+		prev2 = nextTerm;
+		if (i < n)
+			printf("%ld, ", nextTerm);
+		else
+			printf("%ld\n", nextTerm);
+	}
+	return (0);
+}
