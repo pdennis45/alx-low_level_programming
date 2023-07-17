b7e898acd2718f823ae8d3b08e383780e80f105a
pdennis45 <peterdennis45@gmail.com>
2023-07-17 11:47:51 -0700

    0. Define a new type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+readme file for project 0x0E. C - Structures, typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - definition of dog structure
+ *@name: Name of the dog
+ *@age: Age of the dog
+ *@owner: Owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
