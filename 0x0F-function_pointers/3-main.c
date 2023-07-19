71cedaedd45165d3d1bff2843f1382b100a0f1ca
pdennis45 <peterdennis45@gmail.com>
2023-07-19 07:57:47 -0700

    3. a program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,14 +18,11 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	oprt = get_op_func(argv[2]);
-	if (oprt)
-	{
-		printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
-		return (0);
-	}
-	else
+	if (!oprt)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
