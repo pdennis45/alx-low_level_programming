34f8c449e24a6413ddc96250ec0b7df59d99acd7
pdennis45 <peterdennis45@gmail.com>
2023-08-07 12:30:55 -0700

    1. a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	if (text_content)
+	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i];)
 			i++;
